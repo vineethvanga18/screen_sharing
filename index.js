f4b558bc9a7751efc32a596970c38ab78d8b7c68
vineethvanga18 <reddy.8@iitj.ac.in>
2020-10-16 22:58:12 +0530

    modification

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-const WebSocket = require('ws');
-let socket = new WebSocket("ws://localhost:9898/");
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
 
-socket.onopen = function(e) {
-  console.log("[open] Connection established");
-  console.log("Sending to server");
-  socket.send("My name iees Jon");
-};
+app.listen(8000, '0.0.0.0');
 
-socket.onmessage = function(event) {
-  console.log(`[message] Data received from server: ${event.data}`);
-};
-
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    // alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    // e.g. server process killed or network down
-    // event.code is usually 1006 in this case
-    // alert('[close] Connection died');
-    console.log('[close] Connection died');
-  }
-};
-
-socket.onerror = function(error) {
-  console.log(`[error] ${error.message}`);
-};
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+})
