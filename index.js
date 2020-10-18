41363cddbacb2f555a50af799fb5de96f192999e
vineethvanga18 <reddy.8@iitj.ac.in>
2020-10-18 10:41:01 +0530

    more progress

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
-app.listen(8000, '0.0.0.0');
+app.use('/', express.static('public'))
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
+io.on('connection', (socket) => {
+  socket.on('join', (roomId, user_name) => {
+    const roomClients = io.sockets.adapter.rooms[roomId] || { length: 0 }
+    const numberOfClients = roomClients.length
+
+    // These events are emitted only to the sender socket.
+    if (numberOfClients == 0) {
+      console.log(`Creating room ${roomId} and emitting room_created socket event`)
+      socket.join(roomId)
+      socket.emit('room_created', roomId)
+    } else if (numberOfClients == 1) {
+      console.log(`Joining room ${roomId} and emitting room_joined socket event`)
+      socket.join(roomId)
+      socket.emit('room_joined', roomId)
+    } else {
+      console.log(`Can't join room ${roomId}, emitting full_room socket event`)
+      socket.emit('full_room', roomId)
+    }
+  })
+
+  // These events are emitted to all the sockets connected to the same room except the sender.
+  socket.on('start_call', (roomId) => {
+    console.log(`Broadcasting start_call event to peers in room ${roomId}`)
+    socket.broadcast.to(roomId).emit('start_call')
+  })
+  socket.on('webrtc_offer', (event) => {
+    console.log(`Broadcasting webrtc_offer event to peers in room ${event.roomId}`)
+    socket.broadcast.to(event.roomId).emit('webrtc_offer', event.sdp)
+  })
+  socket.on('webrtc_answer', (event) => {
+    console.log(`Broadcasting webrtc_answer event to peers in room ${event.roomId}`)
+    socket.broadcast.to(event.roomId).emit('webrtc_answer', event.sdp)
+  })
+  socket.on('webrtc_ice_candidate', (event) => {
+    console.log(`Broadcasting webrtc_ice_candidate event to peers in room ${event.roomId}`)
+    socket.broadcast.to(event.roomId).emit('webrtc_ice_candidate', event)
+  })
+})
+
+// START THE SERVER =================================================================
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+  console.log(`Express server listening on port ${port}`)
 })
